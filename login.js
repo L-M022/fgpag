807932d9b2af19d28c720739c8c8a4813f477803
Leonel <leonelmona018@gmail.com>
2025-09-18 17:55:24 -0300

    + Base de carrito + Deteccion de dispositivo para css + Nombre y apellido para registrar + Boton cancelar registro/login

--- login.js
+++ login.js
@@ -19,16 +19,16 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 // Registrar usuario nuevo
 window.registrar = async function () {
-    
+
     const nombre = document.getElementById("name").value;
-    const apellido =  document.getElementById("surname").value;
+    const apellido = document.getElementById("surname").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         // Asignar displayName
         const user = userCredential.user;
-        await updateProfile(user, { displayName: nombre+" "+apellido });
+        await updateProfile(user, { displayName: nombre + " " + apellido });
         console.log("Usuario creado:", userCredential.user);
         alert("Registro exitoso: " + userCredential.user.displayName);
     } catch (error) {
@@ -37,14 +37,15 @@ window.registrar = async function () {
 }
 
 // Iniciar sesión
-window.login = async function () {
+window.ingresar = async function () {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
- 
+
         console.log("Logueado:", userCredential.user);
         alert("Bienvenido: " + userCredential.user.displayName);
+         window.location.href = "http://192.168.100.112:5500/index.html"; 
     } catch (error) {
         alert("Error: " + error.message);
     }
@@ -55,18 +56,34 @@ window.logout = async function () {
     await signOut(auth);
     alert("Sesión cerrada");
 }
-
+window.goBack = async function () {
+    var button = document.getElementById("login_register_btn");
+    if (button.innerHTML == "Registrar") {
+        button.innerHTML = "Ingresar";
+        document.getElementById("name").style.display = "none";
+        document.getElementById("surname").style.display = "none";
+        document.getElementById("register_tip").style.display = "block";
+    } else{
+        window.location.href = "http://192.168.100.112:5500/index.html"; 
+    }
+}
+window.logToReg = async function () {
+    document.getElementById("login_register_btn").innerHTML = "Registrar";
+    document.getElementById("login_register_btn").onclick = registrar();
+    document.getElementById("name").style.display = "block";
+    document.getElementById("surname").style.display = "block";
+    document.getElementById("register_tip").style.display = "none"; 
+}
 // Detectar cambios de sesión
 onAuthStateChanged(auth, (user) => {
     if (user) {
-       var  nombreapellido = user.displayName || "";
+        var nombreapellido = user.displayName || "";
         var nombre = nombreapellido.split(" ")[0].split("")[0];
         var apellido = nombreapellido.split(" ")[1].split("")[0];
-        document.getElementById("user").innerHTML =  nombre[0] + apellido[0];
+        document.getElementById("user").innerHTML = nombre[0] + apellido[0];
         console.log("usuario: " + user.displayName);
-        document.getElementById("estado").textContent = "Logueado como: " + user.email;
     } else {
-        document.getElementById("estado").textContent = "No logueado";
+        document.getElementById("login_register_btn").innerHTML = "Ingresar";
     }
 });
 
